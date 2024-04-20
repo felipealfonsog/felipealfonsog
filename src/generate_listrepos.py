9d507a8777b7d3e4722caef1b5eb3c9ee6a45bef
Felipe <f.alfonso@res-ear.ch>
2024-04-20 19:22:05 -0400

    Updates

--- src/generate_listrepos.py
+++ src/generate_listrepos.py
@@ -1,62 +1,51 @@
-import os
 import requests
 
 def generate_readme_and_listrepos(num_repos):
     try:
-        # Obtener el token de acceso desde el secreto
-        token = os.getenv('GH_TOKEN_LRGH_CLASS')
-
-        if not token:
-            raise Exception("GitHub token not found. Make sure to set the GH_TOKEN_LRGH_CLASS environment variable.")
-
-        headers = {
-            'Authorization': f'token {token}'
-        }
-
-        # Hacer una solicitud a la API de GitHub para obtener los repositorios más recientes del usuario
-        response = requests.get(f"https://api.github.com/users/felipealfonsog/repos?sort=updated&per_page={num_repos}", headers=headers)
+        # Make a request to the GitHub API to get the user's most recent repositories
+        response = requests.get(f"https://api.github.com/users/felipealfonsog/repos?sort=updated&per_page={num_repos}")
 
         if not response.ok:
             raise Exception(f"Error fetching repositories: {response.status_code} - {response.reason}")
 
         repos = response.json()
 
-        # Ordenar los repositorios por la fecha de actualización (del más reciente al más antiguo)
+        # Sort the repositories by the last updated date (from most recent to oldest)
         sorted_repos = sorted(repos, key=lambda x: x['updated_at'], reverse=True)
 
-        # Construir el contenido para la lista de repositorios
+        # Build the content for the list of repositories
         repos_content = " \n"
         for repo in sorted_repos:
             repos_content += f"- [{repo['full_name']}]({repo['html_url']})\n"
 
-        # Leer el contenido del archivo README.md
+        # Read the content of the README.md file
         with open('README.md', 'r') as file:
             readme_content = file.read()
 
-        # Encontrar los marcadores de inicio y fin para la sección de repositorios
+        # Find the start and end markers for the repositories section
         start_marker = '<!-- START_SECTION:repos -->'
         end_marker = '<!-- END_SECTION:repos -->'
 
-        # Encontrar los índices de inicio y fin de la sección de repositorios
+        # Find the start and end indices of the repositories section
         start_index = readme_content.find(start_marker)
         end_index = readme_content.find(end_marker)
 
         if start_index == -1 or end_index == -1:
             raise Exception("Markers not found in README.md")
 
-        # Extraer el contenido entre los marcadores
+        # Extract the content between the markers
         existing_content = readme_content[start_index + len(start_marker):end_index]
 
-        # Reemplazar el contenido existente con el nuevo contenido de los repositorios
+        # Replace the existing content with the new repositories content
         new_readme_content = readme_content.replace(existing_content, repos_content)
 
-        # Guardar el nuevo contenido en el archivo README.md
+        # Save the new content to the README.md file
         with open('README.md', 'w') as file:
             file.write(new_readme_content)
 
         print("README.md updated successfully!")
 
-        # Actualizar el contenido del archivo LISTINGREPOS.md
+        # Update the content of the LISTINGREPOS.md file
         with open('LISTINGREPOS.md', 'w') as file:
             file.write(repos_content)
 
@@ -64,5 +53,5 @@ def generate_readme_and_listrepos(num_repos):
     except Exception as e: 
         print(f"Error generating files: {e}")
 
-# Llamar a la función para actualizar README.md con la lista de repositorios
+# Call the function to update README.md with the list of repositories
 generate_readme_and_listrepos(13)
