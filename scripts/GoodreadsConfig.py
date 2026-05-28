e2041d5d33848052734b85d4fdf8369cff097617
Felipe Alfonso González <f.alfonso@res-ear.ch>
2026-05-28 01:10:16 -0400

    Update GoodreadsConfig.py

--- scripts/GoodreadsConfig.py
+++ scripts/GoodreadsConfig.py
@@ -40,10 +40,10 @@ RECENT_READ_SHELF = "read"
 # ============================================================
 # SECTION LIMITS
 # ============================================================
-GLOBAL_SECTION_LIMIT = 25
+GLOBAL_SECTION_LIMIT = 50
 USE_GLOBAL_SECTION_LIMIT = False
 
-CURRENTLY_READING_LIMIT = 25
+CURRENTLY_READING_LIMIT = 50
 RECENT_READ_LIMIT = 75
 
 
